103ddabda7f7b0e887770c17b7f120c9bb570520
kgerov <kgerov@abv.bg>
2015-06-29 13:35:00 +0300

    external library: mvc expressions

--- Twitter/Twitter.Web/Controllers/HomeController.cs
+++ Twitter/Twitter.Web/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Twitter.Data.UnitOfWork;
+using System.Web.Mvc.Expressions;
 
 namespace Twitter.Web.Controllers
 {
@@ -27,9 +24,7 @@ namespace Twitter.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return this.RedirectToAction(x => x.Contact());
         }
 
         public ActionResult Contact()
